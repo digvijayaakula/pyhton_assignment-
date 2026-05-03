b01639ace306dad36d5db37ae7854808d0b2b4d8
digvijayaakula <digvijaya.akula@atenas.in>
2026-05-03 19:31:24 +0530

    assignment8.py

---
+++ assignment8.py/1.py
@@ -0,0 +1,12 @@
+'''Write a program that takes a list of numbers and uses built-in functions to print:
+
+* Maximum value
+* Minimum value
+* Sum of elements
+* Length of the list'''
+
+num = [10, 20, 5, 40]
+print("Maximum:", max(num))    
+print("Minimum:", min(num))
+print("Sum:", sum(num))
+print("Length:", len(num))
